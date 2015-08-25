470774e487aaaa7938f6d2e3472b346d7eb2c5ee
Reuben Poharama <reuben.hubbubstudios.com>
2015-08-25 16:04:13 +1200

    updated sh file

--- clone_addons.sh
+++ clone_addons.sh
@@ -3,7 +3,7 @@
 # Dependencies for VJExperiment
 #command is 'sh' then this file name
 
-cd ../
+cd ../../addons/
 
 git clone https://github.com/neilmendoza/ofxRules.git
 git clone https://github.com/neilmendoza/ofxPostProcessing.git
